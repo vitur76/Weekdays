4d487a8a6e19657d706bd68bb5c7d0dc4d7673fa
Vitalie Urmas <vitalie.urmas@gmail.com>
2024-10-02 16:33:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,43 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        enum Weekday {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            boolean isWeekDay(Weekday wd)
+            {
+               if(wd == Weekday.Monday ||
+                  wd == Weekday.Tuesday ||
+                  wd == Weekday.Wednesday ||
+                  wd == Weekday.Thursday ||
+                  wd == Weekday.Friday )
+                   return true;
+
+               else
+                   return false;
+            }
+
+            boolean isHolyDay(Weekday wd)
+            {
+                if(wd == Weekday.Saturday ||
+                   wd == Weekday.Sunday )
+                    return true;
+
+                else
+                    return false;
+            }
+        }
+
+        for (Weekday wdType: Weekday.values()) {
+            System.out.println(wdType.name() + " " + wdType.ordinal());
+            System.out.println("isWeekDay: " + wdType.isWeekDay(wdType));
+            System.out.println("isHolyDay: " + wdType.isHolyDay(wdType));
         }
     }
 }
